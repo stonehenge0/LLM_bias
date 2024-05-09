1e7913eddc8ed14e19ec759bee80cb2ae4d15368
Emma <em.stein29@gmail.com>
2024-05-09 16:47:03 +0200

    analysis of gendercoded words for original program descriptions

--- code/agentic_communal_traditional_metric/wordlist_matches.py
+++ code/agentic_communal_traditional_metric/wordlist_matches.py
@@ -1,9 +1,63 @@
+# Inspiration for this script is the genderdecoder package: https://github.com/Doteveryone/genderdecoder
+# The package itself is inspired by Kat Matfield's gender decoder: https://github.com/lovedaybrooke/gender-decoder 
 
-import genderdecoder
+import os
+
+import spacy 
+import pandas as pd
+import wordlists # see agentic_communal_traditional_metric/wordlists.py
 
 
-female_text = "child, quiet stuffs"
-male_text = "unreasonable men"
-psych = "Presentation of the study programme In the consecutive Master's programme in Clinical Psychology and Psychotherapy, existing basic knowledge is specifically deepened. In addition to the sound methodological training, the degree programme allows a high degree of specialisation and profile formation. The competences acquired in the Bachelor's programme can be expanded, especially in the field of psychotherapy. The overarching qualification goal of the Master's degree programme is to prepare students for psychotherapeutic work. The students should acquire the following competences, among others: Scientific work (incl. research planning, research methods, evaluation of empirical studies) Interpretation of research results Selection of adequate psychological diagnostic instruments as well as their application and evaluation Therapy planning and implementation of individual interventions Professional communication with patients using therapeutic conversation techniques and interventions Professional written and oral presentation as well as competences for oral patient presentation Ability to critically reflect on one's own psychotherapeutic actions and reception of feedback What you should bring with you In addition to previous knowledge of natural sciences and interest in a degree programme with a high practical component, you should also have a sound knowledge of English. Occupational fields The primary field of activity for graduates of this degree programme is psychotherapy in the outpatient and inpatient care of mental disorders. Other fields of activity include diagnostics and intervention in areas such as counselling facilities, rehabilitation facilities, prevention facilities, (occupational) health care or as an expert witness as well as an administrator in the health care sector and as a psychotherapy researcher"
-print (genderdecoder.assess(female_text)) # I love how their README example alone had like three mistakes in it. 
+nlp = spacy.load("en_core_web_sm")
+
+def preprocess(input_text):
+    """Lowercase everything, remove non alphanumeric characters."""
+    doc = nlp(input_text)
+    cleaned_tokens = [token.text.lower() for token in doc if token.is_alpha]
+
+    return cleaned_tokens
+
+def get_wordlist_matches(in_text):
+    """Finds all matches of gender-coded words and returns male and female coded words."""
+
+
+    # Goes through all the words in the input text and returns that word if it starts with any of the 
+    # gender coded stems from the wordlists script.
+    masculine_coded_words = [word for word in in_text
+        for coded_word in wordlists.masculine_coded_words
+        if word.startswith(coded_word)]
+    
+    feminine_coded_words = [word for word in in_text
+        for coded_word in wordlists.feminine_coded_words
+        if word.startswith(coded_word)]
+    
+    return {
+            "masculine_coded_words": masculine_coded_words,
+            "feminine_coded_words": feminine_coded_words
+            }
+
+def main():
+
+    # Read in the dataframe. Change that this is hardcoded.
+
+    # Define the relative path to the file
+    file_path = os.path.join("data", "program_descriptions.csv")
+
+    # Get the absolute path to the file
+    absolute_file_path = os.path.abspath(file_path)
+
+
+    df = pd.read_csv(absolute_file_path)
+
+    # Apply both functions from above to each entry in the program descriptions column.
+    df['gendercoded words'] = df['program description'].apply(lambda x: preprocess(x))
+    df['gendercoded words'] = df['gendercoded words'].apply(lambda x: get_wordlist_matches(x))
+
+    output_file = "gendercoded_wordlists_uncleaned_results.csv"
+    df.to_csv(output_file, index = False)
+    print("Output has been written to:\t", output_file)
+
+
+if __name__ == '__main__':
+    main()
 
