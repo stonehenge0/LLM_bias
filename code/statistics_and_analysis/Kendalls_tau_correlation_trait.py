b3a0a937007c11d7417169c7355675ec24a5ebe3
Emma <em.stein29@gmail.com>
2024-07-17 03:42:35 +0200

    add Kendalls Tau for trait behavior

--- code/statistics_and_analysis/Kendalls_tau_correlation_trait.py
+++ code/statistics_and_analysis/Kendalls_tau_correlation_trait.py
@@ -24,6 +24,37 @@ def extract_classification(json_string):
         return None
 
 
+##################################################
+# Bringing the trait/behavior metric into the right shape.
+##################################################
+
+trait_original_df = pd.read_csv("results/results_trait_behavior.csv")
+trait_paraphrased_df = pd.read_csv("results/p_results_trait_behavior.csv")
+
+
+## Make original agentic dataframe into the right format.
+# Keep only the id, program name and score for the original df
+columns_to_keep = ["program-id", "program name", "brysbaert score"]
+
+# add a column to identify the original program descriptions later.
+trait_original_df = trait_original_df[columns_to_keep]
+trait_original_df.insert(2, "paraphrased as", "original program description")
+
+
+## Make paraphrased agentic dataframe into the right format.
+columns_to_keep = [
+    "program-id",
+    "program name",
+    "paraphrased as",
+    "brysbaert score",
+]
+
+trait_paraphrased_df = trait_paraphrased_df[columns_to_keep]
+
+# Combine the two dataframes
+trait_df = pd.concat([trait_original_df, trait_paraphrased_df])
+
+
 #################################################
 # Now we do the same for LLAMA.
 ###################################################
@@ -65,12 +96,12 @@ llama_df = pd.concat([llama_original_df, llama_paraphrased_df])
 ##################################################################
 
 # Convert categorial data to ranks from the LLAMA classification.
-category_to_rank = {
-    "strongly male oriented": 1,
-    "moderately male oriented": 2,
+category_to_rank = {  # note that here the LLM mappings are in reverse to agentic/communal, because for this metric, a higher score indicates male, not the other way around
+    "strongly male oriented": 5,
+    "moderately male oriented": 4,
     "neutral": 3,
-    "moderately female oriented": 4,
-    "strongly female oriented": 5,
+    "moderately female oriented": 2,
+    "strongly female oriented": 1,
 }
 llama_df["LLAMA classification"] = llama_df["LLAMA classification"].map(
     category_to_rank
@@ -81,9 +112,7 @@ print("Data with ranks:")
 print(llama_df)
 
 # Calculate Kendall's tau
-tau, p_value = kendalltau(
-    llama_df["LLAMA classification"], agentic_df["agentic communal score"]
-)
+tau, p_value = kendalltau(llama_df["LLAMA classification"], trait_df["brysbaert score"])
 
-print(f"\nKendall's tau: {tau}")
-print(f"P-value: {p_value}")
+print(f"\nKendall's tau: {tau}")  # 0.2099093177714554
+print(f"P-value: {p_value}")  # 0.038529314992439445
